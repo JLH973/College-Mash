6075b3e20fa4c4f8f27726b0958f971996fa45db
Jay Humphreys <57715208+JLH973@users.noreply.github.com>
2022-02-25 12:54:53 -0500

    update rand if

--- script.js
+++ script.js
@@ -50,9 +50,10 @@ let runTable = 1;
 let rand1 = Math.floor(Math.random() * names.length);
 let rand2 = Math.floor(Math.random() * names.length);
 while(rand1 == rand2){
-    if (rand1 == rand2)
+    if (rand1 == rand2){
     while (rand2 == rand1) {
     rand2 = Math.floor(Math.random() * names.length);
+    }
     }
 }
 
