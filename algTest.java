8733b79386c7b486484f9b1939ff9851e3561159
Jay Humphreys <57715208+JLH973@users.noreply.github.com>
2022-02-25 08:15:25 -0500

    Update algTest.java

--- algTest.java
+++ algTest.java
@@ -1,24 +1,18 @@
-class algTest
-{
-    static main(args)
-    {
-        var numA = -18;
-        var numB = 100;
+
+        var scoreA = -18;
+        var scoreB = 100;
         var k = 10;
-        console.log("Original: " + numA);
-        console.log("Original: " + numB);
-        var Qa = Math.pow(10,(numA / 400.0));
-        var Qb = Math.pow(10,(numB / 400.0));
+        console.log("Original: " + scoreA);
+        console.log("Original: " + scoreB);
+        var Qa = Math.pow(10,(scoreA / 400.0));
+        var Qb = Math.pow(10,(scoreB / 400.0));
         var Bexpect = Qa / (Qa + Qb);
         var Aexpect = Qb / (Qa + Qb);
-        var AwinCalcA = numA - k * (1 - Aexpect);
-        var AwinCalcB = numB - k * (0 - Bexpect);
-        var BwinCalcA = numA - k * (0 - Aexpect);
-        var BwinCalcB = numB - k * (1 - Bexpect);
+        var AwinCalcA = scoreA - k * (1 - Aexpect);
+        var AwinCalcB = scoreB - k * (0 - Bexpect);
+        var BwinCalcA = scoreA - k * (0 - Aexpect);
+        var BwinCalcB = scoreB - k * (1 - Bexpect);
         console.log("A wins: A:" + parseInt(AwinCalcA));
         console.log("\tB: " + parseInt(AwinCalcB));
         console.log("B wins: A:" + parseInt(BwinCalcA));
         console.log("\tB: " + parseInt(BwinCalcB));
-    }
-}
-algTest.main([]);
